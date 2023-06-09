c3cd74fdb772136580ffcd2795430c1bf4753c59
ml1413 <ml1413sam@gmail.com>
2023-06-10 01:49:07 +0300

    55c

--- app/src/main/java/com/ooommm/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/ooommm/ui/fragments/SettingsFragment.kt
@@ -1,8 +1,16 @@
 package com.ooommm.ui.fragments
 
+import android.view.Menu
+import android.view.MenuInflater
 import com.ooommm.clontelegramm3.R
 
 class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
+    override fun onResume() {
+        super.onResume()
+        setHasOptionsMenu(true)
+    }
 
-
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        activity?.menuInflater?.inflate(R.menu.settings_action_menu, menu)
+    }
 }
